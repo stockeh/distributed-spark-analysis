e7a851e2c77ed00ba46e4aea52a370c549f4bd2b
Evan Steiner <evanjs@atlanta.cs.colostate.edu>
2019-04-23 20:43:51 -0600

    changed run file a bit, fixed time of day calculations

--- src/main/scala/cs455/spark/basic/MostTimeOfDay.scala
+++ src/main/scala/cs455/spark/basic/MostTimeOfDay.scala
@@ -6,19 +6,27 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.expressions.Window
 
 object MostTimeOfDay {
+  val ORDER_PROD_SET = "order_*.csv"
+  val ORDERS = "orders.csv"
+  val PRODUCTS = "products.csv"
+
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.ERROR)
     Logger.getLogger("akka").setLevel(Level.ERROR)
 
-    val orders = args(0)
-    val orders_products = args(1)
-    val products = args(2)
-    val output = args(3)
+    val directory = args(0)
+    val output = args(1)
+    val orders_products = directory + ORDER_PROD_SET
+    val orders = directory + ORDERS
+    val products = directory + PRODUCTS
+    var master = "yarn"
+
+    if(args.length > 2) master = "local"
 
     val spark = SparkSession
       .builder
-      .appName("Most Time of Day")
-      .master("local")
+      .appName("TopFoodByHour")
+      .master(master)
       .getOrCreate()
 
     findMostCommonByHour(spark, orders, orders_products, products, output)
@@ -34,11 +42,9 @@ object MostTimeOfDay {
 
     //joined order data
     var joined = orders.join(order_products, Seq("order_id"))
-    //joined.show(false)
 
     //select necessary columns from the main dataset
     joined = joined.selectExpr("order_id", "order_hour_of_day", "product_id")
-    joined.show(false)
 
     //collect counts for each occurence of each product at each hour of the day
     joined = joined.groupBy("product_id","order_hour_of_day").count()
@@ -47,9 +53,10 @@ object MostTimeOfDay {
     //drop all but top value for each column
     joined = joined.withColumn("row", row_number().over(Window.partitionBy(col("order_hour_of_day")).orderBy(col("count").desc)))
       .where(col("row") === 1).drop("row")
-    joined.show()
+
+    //join with products in order to output product names
     val top_products = joined.join(products, Seq("product_id"), "inner").selectExpr("order_hour_of_day", "count", "product_name")
-    top_products.show()
-    joined.rdd.saveAsTextFile(output)
+
+    top_products.rdd.repartition(1).saveAsTextFile(output)
   }
 }
