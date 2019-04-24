66bb0db2eb4e410d006e8e12a94fe07540c4f976
Evan Steiner <evanjs@santa-fe.cs.colostate.edu>
2019-04-24 17:49:37 -0600

    almost done with food matching?

--- src/main/scala/cs455/spark/basic/BestFoodMatch.scala
+++ src/main/scala/cs455/spark/basic/BestFoodMatch.scala
@@ -2,7 +2,7 @@ package cs455.spark.basic
 
 import cs455.spark.basic.MostTimeOfDay.findMostCommonByHour
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
 
 object BestFoodMatch {
   val USDA_PRODUCTS = "Products.csv"
@@ -28,23 +28,29 @@ object BestFoodMatch {
     findBestMatches(spark, usda_products, insta_products, output)
   }
 
+class MatchTuple()
 
   def findBestMatches(spark : SparkSession, usda_products : String, insta_products : String, output : String): Unit = {
-    val usda = spark.read.format("csv").option("header", "true").load(usda_products).rdd
-    val instacart = spark.read.format("csv").option("header", "true").load(insta_products).rdd
+    val usda = spark.read.format("csv").option("header", "true").load(usda_products).selectExpr("NDB_Number","long_name").rdd
+    val instacart = spark.read.format("csv").option("header", "true").load(insta_products).selectExpr("product_id","product_name").rdd
 
     val cartesian_rdd = instacart.cartesian(usda)
-
     val cartesian_result = cartesian_rdd.map(row =>{
       val insta_product = row._1.get(1).toString.toUpperCase.split(" ")
       val usda_product = row._2.get(1).toString.toUpperCase.split(" ")
-
       val cart_val = cartesianFilter(insta_product, usda_product)
-      if (cart_val > 0) (row._1, row._2, cart_val)
+      if (cart_val > .1) Tuple2( Array(row._1.get(0),row._1.get(1)), Array(row._2.get(0),row._2.get(1), cart_val))
+      else Tuple2(Array(),Array())
+    }).filter(row => row._1.nonEmpty)
+    cartesian_result.foreach(arr => {
+      println(arr._1.mkString(" "))
+      println(arr._2.mkString(" "))
     })
-
-    cartesian_result.foreach(println)
-
+    val reduced_cartesian = cartesian_result.reduceByKey((a, b) => if (a(2).toString.toDouble > b(2).toString.toDouble) a else b )
+    reduced_cartesian.foreach(arr => {
+      println(arr._1.mkString(" "))
+      println(arr._2.mkString(" "))
+    })
   }
 
   def printWithSpace(): Unit = {
