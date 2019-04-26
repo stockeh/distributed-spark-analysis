d66f7ba0d1709210734d2bb67a8876d720f6f649
gamerpuppy <keegan.millard@gmail.com>
2019-04-25 18:28:35 -0600

    adsf

--- src/main/scala/cs455/spark/basic/FirstOrder.scala
+++ src/main/scala/cs455/spark/basic/FirstOrder.scala
@@ -23,12 +23,14 @@ object FirstOrder {
 
     import spark.implicits._
 
-    val directory = args(0)
-    val output = args(1)
+    val instacartFolderPath = args(0)
+    val bfpdFolderPath = args(1)
+    val linkPath = args(2)
+    val output = args(3)
 
     // Read in order data as DataFrame
     val orders = spark.read.format("csv").option("header", "true")
-      .load(directory + ORDER_PROD_SET)
+      .load(instacartFolderPath + ORDER_PROD_SET)
 
     // Get all first item products added to cart
     val filtered = orders.filter($"add_to_cart_order" === 1)
@@ -36,11 +38,18 @@ object FirstOrder {
 
     // Read in product data as a DataFrame
     val products = spark.read.format("csv").option("header", "true")
-      .load(directory + PRODUCTS)
+      .load(instacartFolderPath + PRODUCTS)
 
     // Join the two DataFrames on product_id
     val joint = filtered.join(products, "product_id").drop("aisle_id", "department_id")
 
-    joint.rdd.coalesce(1).saveAsTextFile(output)
+    val link = spark.read.format("csv").option("header", "true")
+      .load(linkPath)
+
+    val jointLink = joint.join(link, "product_name")
+
+    jointLink.rdd.take(10).foreach(println)
+
+//    joint.rdd.coalesce(1).saveAsTextFile(output)
   }
 }
