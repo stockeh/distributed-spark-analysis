d7959bbbdc3b2eabb2231b80bd86f7a78ed7f339
Evan Steiner <evanjs@trenton.cs.colostate.edu>
2019-04-26 16:44:03 -0600

    collaborative filtering kinda working but not

--- src/main/scala/cs455/spark/basic/CollaborativeFilteringRecommender.scala
+++ src/main/scala/cs455/spark/basic/CollaborativeFilteringRecommender.scala
@@ -5,6 +5,8 @@ import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.recommendation.ALS
 import org.apache.spark.ml.recommendation.ALS.Rating
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+import org.apache.spark.storage.StorageLevel
 
 import scala.collection.{Map, mutable}
 import scala.collection.mutable.ArrayBuffer
@@ -37,74 +39,66 @@ object CollaborativeFilteringRecommender {
   }
 
   def recommend(spark : SparkSession, str_orders : String, str_orders_products : String, str_products : String, output : String): Unit = {
-    val orders = spark.read.format("csv").option("header", "true").load(str_orders)
-    val order_products = spark.read.format("csv").option("header", "true").load(str_orders_products)
+
+    var schema = StructType(Array(StructField("user_id", IntegerType, nullable = true), StructField("order_id", IntegerType, nullable = true)))
+    var schema2 = StructType(Array(StructField("product_id", IntegerType, nullable = true), StructField("order_id", IntegerType, nullable = true)))
+
+    val orders = spark.read.format("csv").option("header", "true").schema(schema).load(str_orders)
+    val order_products = spark.read.format("csv").option("header", "true").schema(schema2).load(str_orders_products)
     import spark.implicits._
     //load product data
-    val products = spark.read.format("csv").option("header", "true").load(str_products)
+//    val products = spark.read.format("csv").option("header", "true").load(str_products)
 
     //join orders datasets
     val orders_set = orders.join(order_products, Seq("order_id"))
 
-    //map user id to product ids
-    var orders_rdd = orders_set.rdd.map(row => {
-      (row.getAs("user_id").toString.toInt, ArrayBuffer(row.getAs("product_id").toString.toInt))
-    })
+    val user_counts = orders_set.groupBy("user_id").count()
+      .orderBy($"count".desc).limit(100).withColumnRenamed("count", "total_purchased")
 
-    //reduce by key on users to get list of prodcuts for each user
-    orders_rdd = orders_rdd.reduceByKey((p1,p2) => {
-     val combined = p1 ++= p2
-      combined
-    })
-    //user,list(product_ids)
-    val users_products = orders_rdd.mapValues(list => {
-      var fulllist = new Array[Int](49689)
-      list.foreach(e => fulllist(e) += 1)
-      fulllist.toList
-    })
-    val user_to_product = users_products.flatMap(v => {
-      var list = ArrayBuffer[Rating[Int]]()
-      for(i <- v._2.indices) {
-        list += Rating(v._1, i, v._2(i))
-      }
-      list
-    }).toDF("user_id", "product_id", "rating")
-    user_to_product.show(false)
+    val product_counts = orders_set.groupBy("user_id", "product_id").count()
 
+    val users_products_counts = user_counts.join(product_counts, Seq("user_id"))
+      .withColumn("rating", $"count" / $"total_purchased").drop("count", "total_purchased")
+//    users_products_counts.printSchema()
+//    users_products_counts.show(false)
     val als = new ALS()
-      .setMaxIter(1)
+      .setMaxIter(5)
       .setRegParam(0.01)
       .setUserCol("user_id")
       .setItemCol("product_id")
       .setRatingCol("rating")
+//      .setColdStartStrategy("drop")
 
 
-    val Array(training, test) = user_to_product.randomSplit(Array(.0005,.9995))
+    var Array(training, test) = users_products_counts.randomSplit(Array(.8,.2))
+//    training = training.persist(StorageLevel.MEMORY_ONLY)
+//    test = test.persist(StorageLevel.MEMORY_ONLY)
     val model = als.fit(training)
+    model.save(output + "/model/")
 
-    model.setColdStartStrategy("drop")
-    val predictions = model.transform(test)
-
-    val evaluator = new RegressionEvaluator()
-      .setMetricName("rmse")
-      .setLabelCol("rating")
-      .setPredictionCol("prediction")
-    val rmse = evaluator.evaluate(predictions)
-    println("Root-mean-square-error = " + rmse)
-
-    val topProductRecsPerUser = model.recommendForAllUsers(10)
-    topProductRecsPerUser.rdd.saveAsTextFile(output+"topProductsRecsPerUser/")
-
-    val topUserRecsPerProduct = model.recommendForAllItems(10)
-    topUserRecsPerProduct.rdd.saveAsTextFile(output+"topUserRecsPerProduct/")
-
-    val users = user_to_product.select(als.getUserCol).distinct().limit(3)
-    val topProductRecsPerUserSubset = model.recommendForUserSubset(users, 10)
-    topProductRecsPerUserSubset.rdd.saveAsTextFile(output+"topProductRecsPerUserSubset/")
-
-    val items = user_to_product.select(als.getItemCol).distinct().limit(3)
-    val topUserRecsPerProductSubset = model.recommendForItemSubset(items, 10)
-    topUserRecsPerProductSubset.rdd.saveAsTextFile(output+"topUserRecsPerProductSubset/")
+    //model.setColdStartStrategy("drop")
+//    val predictions = model.transform(test)
+//
+//    val evaluator = new RegressionEvaluator()
+//      .setMetricName("rmse")
+//      .setLabelCol("rating")
+//      .setPredictionCol("prediction")
+//    val rmse = evaluator.evaluate(predictions)
+//    println("Root-mean-square-error = " + rmse)
+//
+//    val topProductRecsPerUser = model.recommendForAllUsers(10)
+//    topProductRecsPerUser.rdd.saveAsTextFile(output+"topProductsRecsPerUser/")
+//
+//    val topUserRecsPerProduct = model.recommendForAllItems(10)
+//    topUserRecsPerProduct.rdd.saveAsTextFile(output+"topUserRecsPerProduct/")
+//
+//    val users = users_products_counts.select(als.getUserCol).distinct().limit(3)
+//    val topProductRecsPerUserSubset = model.recommendForUserSubset(users, 10)
+//    topProductRecsPerUserSubset.rdd.saveAsTextFile(output+"topProductRecsPerUserSubset/")
+//
+//    val items = users_products_counts.select(als.getItemCol).distinct().limit(3)
+//    val topUserRecsPerProductSubset = model.recommendForItemSubset(items, 10)
+//    topUserRecsPerProductSubset.rdd.saveAsTextFile(output+"topUserRecsPerProductSubset/")
   }
 
 
